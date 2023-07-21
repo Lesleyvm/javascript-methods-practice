27c4dbb999f88cbc60052e633a98bbab2f38ac3c
Lesleyvm <lvanmusscher@gmail.com>
2023-07-21 20:17:21 +0200

    nagekeken

--- string-methods.js
+++ string-methods.js
@@ -13,7 +13,7 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
-
+console.log(fruit.length > vegetable.length)
 
 /* Opdracht 2 */
 //  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
@@ -21,29 +21,37 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
 
+const lastIndexFruit = fruit.length - 1;
+const lastIndexVegetable = vegetable.length - 1;
 
+const lastFruit = fruit.substring(lastIndexFruit);
+const lastVegetable = vegetable.substring(lastIndexVegetable);
+
+console.log(lastFruit, lastVegetable);
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
 // ---- Verwachte uitkomst: "De kat krabt de krullen van de trap"
 
 const userInput = "  De kat krabt de krullen van de trap ";
-
-
+console.log(userInput.trim())
 
 /* Opdracht 4 */
 // Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
 // ---- Verwachte uitkomst: true
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
-
+console.log(story.includes("Fantastisch"))
 
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
 
+const endOfSentence = story.indexOf('.');
 
+const sentence = story.substring(0, endOfSentence + 1);
+console.log(sentence);
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -53,7 +61,7 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 // ---- Verwachte uitkomst: "€35,59"
 
 const price = "$35.59";
-
+console.log(price.replaceAll("$", "€").replaceAll(".", ","))
 
 
 
